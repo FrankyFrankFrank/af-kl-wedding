79c93afb2877de6af5d5b41e6fe1b3ae84e89112
Adam Frank <a.frank@vehikl.com>
2017-05-12 15:11:50 -0400

    Revert "Revert "Revert "User Wedding Actions"""
    This reverts commit d46a44b13f29476d0e4443b91b13be65da3f7438.

--- resources/views/rsvp.blade.php
+++ resources/views/rsvp.blade.php
@@ -10,9 +10,9 @@
 		</div>
 	</div>
 </div>
-<div class="container">
-	<div class="columns">
-		<div class="column">
+<section class="hero is-fullheight">
+	<div class="hero-body has-text-centered">
+		<div class="container">
 			<h1>RSVP</h1>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -22,33 +22,6 @@
 			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		</div>
 	</div>
-	<div class="columns">
-		<div class="column">
-		@if(auth()->user() == 'unresponded')
-			{{ Form::open(['action' => 'RSVPController@submit']) }}
-				<div class="field">
-				  <label class="label" for="status">I will be</label>
-				  <p class="control">
-				    <span class="select">
-				      <select id="status" name="status">
-				        <option value="attending">Attending</option>
-				        <option value="declined">Not Attending</option>
-				      </select>
-				    </span>
-				  </p>
-				</div>
-				<div class="field">
-				  <p class="control">
-				    <button class="button is-primary">RSVP</button>
-				  </p>
-				</div>
-			{{ Form::close() }}
-		@else
-			<p>{{ auth()->user()->status }}</p>
-		@endif
-
-		</div>
-	</div>
-</div>
+</section>
 
 @endsection
