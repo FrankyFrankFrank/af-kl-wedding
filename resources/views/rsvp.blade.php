f4a98b695a3b80e9f494792edc0e7328760f505a
Adam Frank <a.frank@vehikl.com>
2017-05-17 15:30:22 -0400

    Flash and Errors RSVP Form

--- resources/views/rsvp.blade.php
+++ resources/views/rsvp.blade.php
@@ -25,25 +25,35 @@
         </div>
 
         <div class="column">
-            {!! Form::open(['url' => 'foo/bar']) !!}
+            {!! Form::open(['url' => '/rsvp']) !!}
+                <div class="field">
+                    <label class="label" for="firstname">First Name</label>
+                    <p class="control">
+                        <input type="text" id="firstname" name="firstname" class="input" value="{{ old('firstname') }}"/>
+                    </p>
+                    @if($errors->has('firstname'))
+                        <p class="help is-danger">{{ $errors->first('firstname') }}</p>
+                    @endif
+                </div>
+                <div class="field">
+                    <label class="label" for="lastname">Last Name</label>
+                    <p class="control">
+                        <input type="text" id="lastname" name="lastname" class="input" value="{{ old('lastname') }}"/>
+                    </p>
+                    @if($errors->has('lastname'))
+                        <p class="help is-danger">{{ $errors->first('lastname') }}</p>
+                    @endif
+                </div>
                 <div class="field">
                     <label class="label" for="rsvp_number">Your RSVP Code</label>
                     <p class="control">
-                        <input type="text" id="rsvp_number" name="rsvp_number" class="input"/>
+                        <input type="text" id="rsvp_number" name="rsvp_number" class="input" value="{{ old('rsvp_number') }}"/>
                     </p>
+                    @if($errors->has('rsvp_number'))
+                        <p class="help is-danger">{{ $errors->first('rsvp_number') }}</p>
+                    @endif
                 </div>
-                <div class="field">
-                    <label class="label" for="firstname">First Name on Invitation</label>
-                    <p class="control">
-                        <input type="text" id="firstname" name="firstname" class="input"/>
-                    </p>
-                </div>
-                <div class="field">
-                    <label class="label" for="lastname">Last Name on Invitation</label>
-                    <p class="control">
-                        <input type="text" id="lastname" name="lastname" class="input"/>
-                    </p>
-                </div>
+                {{ Form::submit('RSVP', ['class'=> 'button is-primary']) }}
             {!! Form::close() !!}
         </div>
     </div>
