4fc3391c1131a4f0b6bd6d7637935a753cb64606
Adam Frank <a.frank@vehikl.com>
2017-05-17 15:30:35 -0400

    Validation submit rsvp

--- app/Http/Controllers/Auth/RSVPController.php
+++ app/Http/Controllers/Auth/RSVPController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use Validator;
 
 class RSVPController extends Controller
 {
@@ -14,6 +15,19 @@ class RSVPController extends Controller
 
     public function submit(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'rsvp_number' => 'required|numeric',
+            'firstname' => 'required',
+            'lastname' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/rsvp')
+                ->withErrors($validator)
+                ->withInput();
+        }
+        $user = User::where('rsvp_number', $request->rsvp_number)->get();
+
         return view('rsvp');
     }
 }
