c5f0d657a531a512c3fa04829e5465da92f1c07d
Adam Frank <a.frank@vehikl.com>
2017-05-25 18:53:36 -0400

    Padding on section container

--- resources/views/rsvp.blade.php
+++ resources/views/rsvp.blade.php
@@ -10,61 +10,66 @@
 		</div>
 	</div>
 </div>
-<div class="container">
+<div class="section">
+    <div class="container">
+        <div class="columns">
 
-    <div class="columns">
+            <div class="column">
+                <h1>RSVP</h1>
 
-        <div class="column">
-            <h1>RSVP</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
+                @if(isset($user))
+                    <p>Found User</p>
+                    @endif
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
 
-        <div class="column">
-            {!! Form::open(['url' => '/rsvp']) !!}
-                <div class="field">
-                    <label class="label" for="firstname">First Name</label>
-                    <p class="control">
-                        <input type="text" id="firstname" name="firstname" class="input" value="{{ old('firstname') }}"/>
-                    </p>
-                    @if($errors->has('firstname'))
-                        <p class="help is-danger">{{ $errors->first('firstname') }}</p>
-                    @endif
-                </div>
-                <div class="field">
-                    <label class="label" for="lastname">Last Name</label>
-                    <p class="control">
-                        <input type="text" id="lastname" name="lastname" class="input" value="{{ old('lastname') }}"/>
-                    </p>
-                    @if($errors->has('lastname'))
-                        <p class="help is-danger">{{ $errors->first('lastname') }}</p>
-                    @endif
-                </div>
-                <div class="field">
-                    <label class="label" for="rsvp_number">Your RSVP Code</label>
-                    <p class="control">
-                        <input type="text" id="rsvp_number" name="rsvp_number" class="input" value="{{ old('rsvp_number') }}"/>
-                    </p>
-                    @if($errors->has('rsvp_number'))
-                        <p class="help is-danger">{{ $errors->first('rsvp_number') }}</p>
-                    @endif
-                </div>
-                @if($errors->first('guest_not_found'))
+            <div class="column">
+                {!! Form::open(['url' => '/rsvp']) !!}
                     <div class="field">
-                        <p class="help is-danger">Guest Not Found</p>
+                        <label class="label" for="firstname">First Name</label>
+                        <p class="control">
+                            <input type="text" id="firstname" name="firstname" class="input" value="{{ old('firstname') }}"/>
+                        </p>
+                        @if($errors->has('firstname'))
+                            <p class="help is-danger">{{ $errors->first('firstname') }}</p>
+                        @endif
                     </div>
-                @endif
-                @if($errors->first('mismatch'))
                     <div class="field">
-                        <p class="help is-danger">Guest Name Does Not Match RSVP Code</p>
+                        <label class="label" for="lastname">Last Name</label>
+                        <p class="control">
+                            <input type="text" id="lastname" name="lastname" class="input" value="{{ old('lastname') }}"/>
+                        </p>
+                        @if($errors->has('lastname'))
+                            <p class="help is-danger">{{ $errors->first('lastname') }}</p>
+                        @endif
                     </div>
-                @endif
-                {{ Form::submit('RSVP', ['class'=> 'button is-primary']) }}
-            {!! Form::close() !!}
+                    <div class="field">
+                        <label class="label" for="rsvp_number">Your RSVP Code</label>
+                        <p class="control">
+                            <input type="text" id="rsvp_number" name="rsvp_number" class="input" value="{{ old('rsvp_number') }}"/>
+                        </p>
+                        @if($errors->has('rsvp_number'))
+                            <p class="help is-danger">{{ $errors->first('rsvp_number') }}</p>
+                        @endif
+                    </div>
+                    @if($errors->first('guest_not_found'))
+                        <div class="field">
+                            <p class="help is-danger">Guest Not Found</p>
+                        </div>
+                    @endif
+                    @if($errors->first('mismatch'))
+                        <div class="field">
+                            <p class="help is-danger">Guest Name Does Not Match RSVP Code</p>
+                        </div>
+                    @endif
+                    {{ Form::submit('RSVP', ['class'=> 'button is-primary']) }}
+                {!! Form::close() !!}
+            </div>
         </div>
     </div>
 </div>
